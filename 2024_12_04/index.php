6896705464cee45a84053d090ca905d45f896d0c
Prasad Wijerathna <139612338+prasadwijerathna@users.noreply.github.com>
2024-12-08 17:26:45 +0000

    12_04

---
+++ 2024_12_04/index.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <a href="gettable.php">Gettable</a><br>
+    
+
+
+    
+</body>
+</html>
