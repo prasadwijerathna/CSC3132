afd4e20def2ddedf59bcb4a553bdc560cbcbb5d5
Prasad Wijerathna <139612338+prasadwijerathna@users.noreply.github.com>
2024-12-19 08:32:21 +0000

    conf.php

---
+++ 2024_12_13/conf.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+	define('HOST', '127.0.0.1:3306');
+	define('USERNAME', 'root');
+	define('PWD', 'mariadb');
+	define('DB', 'icae02');
+	
+	
+
+
+	$connection = mysqli_connect(HOST,USERNAME,PWD,DB);
+?>
